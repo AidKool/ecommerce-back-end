926ae87ecf0713e0ceb44726a171e970921a970f
jordi <jordi.lanau@yahoo.com>
2022-01-28 17:10:30 +0000

    Add model schemas

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,20 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    tagName: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
