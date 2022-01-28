92f526423672c65a004f063c5d57780cba4217d2
jordi <jordi.lanau@yahoo.com>
2022-01-28 21:51:33 +0000

    Add route to create categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,8 +28,18 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    await Category.create({
+      category_name: req.body.category_name,
+    });
+    Category.findOne({
+      where: { category_name: req.body.category_name },
+    }).then((categoryData) => res.status(201).json(categoryData));
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
