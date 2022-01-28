ddbf65061508e74ebba8dcddec6e00d2b0b02d99
jordi <jordi.lanau@yahoo.com>
2022-01-28 22:05:29 +0000

    Add route to update categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,12 +42,26 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    await Category.update(req.body, { where: { id: req.params.id } });
+    const categoryData = await Category.findOne({
+      where: { id: req.params.id },
+    });
+    res.status(201).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    //
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
