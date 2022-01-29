ed4027c2a47d7852d720034ccf5d8edee20f52d5
jordi <jordi.lanau@yahoo.com>
2022-01-29 22:05:03 +0000

    Add routes to view all products and view individual products by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  // find all products and their associated Category and Tag data
+  try {
+    const productsData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productsData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id` including its associated Category and Tag data
+  try {
+    const { id } = req.params;
+    const productsData = await Product.findByPk(id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productsData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  // create a new product
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -29,12 +45,10 @@ router.post('/', (req, res) => {
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
+        const productTagIdArr = req.body.tagIds.map((tag_id) => ({
+          product_id: product.id,
+          tag_id,
+        }));
         return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
