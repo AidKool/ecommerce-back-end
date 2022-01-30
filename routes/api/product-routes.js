6a59e98f04821d6cbe4e0ccd8285d8adfa80ed51
jordi <jordi.lanau@yahoo.com>
2022-01-30 12:48:16 +0000

    Complete route to update products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -46,12 +46,13 @@ router.post('/', (req, res) => {
           product_id: product.id,
           tag_id,
         }));
-        return ProductTag.bulkCreate(productTagIdArr);
+        ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
-      res.status(200).json(product);
+      // res.status(200).json(product);
+      return product;
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    .then((productTagIds) => res.status(201).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -61,27 +62,30 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category: req.body.category,
+  }, {
     where: {
       id: req.params.id,
     },
   })
-    .then((product) => {
+    .then(() => 
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
+       ProductTag.findAll({ where: { product_id: req.params.id } })
+    )
     .then((productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
+        .map((tag_id) => ({
             product_id: req.params.id,
             tag_id,
-          };
-        });
+          }));
       // figure out which ones to remove
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
